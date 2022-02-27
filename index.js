329207862ceaf30f68e91fbf9934a2c7d4693e2e
Trey Herrera <therrera152@gmail.com>
2022-02-27 10:46:02 -0600

    commit tree eb6fc7353478098d4906bc1a62fa271ccf93bb97

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(toDo = "roller-skate") {
+return `This Saturday, I want to ${toDo}!`
+//saturdayFun("bathe my dog");
+}
+//saturdayFun("bathe my dog")
+
+
+
+function mondayWork(goTo = "go to the office") {
+    return `This Monday, I will ${goTo}.`
+
+}
+//ondayWork("work from home")
+
+
+function wrapAdjective(result = "*") {
+    return function (emphatic = " a hard worker"){
+   return `You are ${result}${emphatic}${result}!`
+    }
+    }
+        
